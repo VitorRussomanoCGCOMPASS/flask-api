acd68ec4f3b415581b17f3d7de5b7aa6366aabcd
VitorRussomanoCGCOMPASS <Vitor.Ibanez@cgcompass.com>
2022-11-29 15:13:36 -0300

    Get by date

--- api/routes/market_index.py
+++ api/routes/market_index.py
@@ -13,46 +13,43 @@ dateargs_schema = DateargsSchema()
 @marketindex_blueprint.route("/", methods=["GET"])
 def get_marketindex():
     args = request.args
-
+    start_date = args.get("start_date", type=str)
+    end_date = args.get("end_date", type=str)
     index = args.get("index", type=str)
 
+    if not start_date:
+
+        if not index:
+            result = MarketIndex.query.all()
+            result = marketindex_schema.dump(result, many=True)
+            return jsonify(result), 200
+
+        if index:
+            result = MarketIndex.query.filter_by(index=index)
+            result = marketindex_schema.dump(result, many=True)
+            return jsonify(result), 200
+
+    errors = dateargs_schema.validate({"start_date": start_date, "end_date": end_date})
+    if errors:
+        return jsonify({"error": "Bad Request", "message": errors}), 400
+
+    if end_date is None:
+        end_date = start_date
+
     if not index:
-        result = MarketIndex.query.all()
+        result = MarketIndex.query.filter(
+            MarketIndex.date.between(start_date, end_date)
+        ).all()
         result = marketindex_schema.dump(result, many=True)
         return jsonify(result), 200
 
     if index:
-        result = MarketIndex.query.filter_by(index=index)
+        result = (
+            MarketIndex.query.filter_by(index=index)
+            .filter(MarketIndex.date.between(start_date, end_date))
+            .all()
+        )
         result = marketindex_schema.dump(result, many=True)
         return jsonify(result), 200
 
     return jsonify({"error": "Bad Request", "message": "#####"}), 400
-
-
-@marketindex_blueprint.route("/date", methods=["GET"])
-def get_marketindex_date():
-    args = request.args
-    start_date = args.get("start_date", type=str)
-    end_date = args.get("end_date", type=str)
-    
-    errors = dateargs_schema.validate({"start_date": start_date, "end_date": end_date})
-    if errors:
-        return jsonify({"error":"Bad Request", "message":errors}) , 400
-
-    result = MarketIndex.query.filter(MarketIndex.date.between(start_date,end_date)).all()  
-
-    result = marketindex_schema.dump(result,many=True)
-    return jsonify(result), 200
-
-
-""" 
-
-    result = MarketIndex.query.filter_by(index=index).all()
-    try:
-        result = marketindex_schema.dump(result, many=True)
-    except ValueError:
-        result = marketindex_schema.dump(result)
-    return jsonify(result), 200
-
-
- """
