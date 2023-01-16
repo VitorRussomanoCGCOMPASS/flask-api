fd5d92041e23321f39539a3a8afbcbd91ad21f31
VitorRussomanoCGCOMPASS <Vitor.Ibanez@cgcompass.com>
2023-01-16 18:12:57 -0300

    updated setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ import setuptools
 
 setuptools.setup(
     name='flask-api',
-    url='https://github.com/VitorRussomanoCGCOMPASS/flask-api',
+    url='https://github.com/VitorRussomanoCGCOMPASS/flask-api.git',
     packages=['flask-api']
 )
