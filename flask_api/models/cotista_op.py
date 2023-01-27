0a3afbab12bd59943c65fe8fe0fb9a161b9e9423
VitorRussomanoCGCOMPASS <Vitor.Ibanez@cgcompass.com>
2023-01-27 18:40:21 -0300

    full cotista_op

--- flask_api/models/cotista_op.py
+++ flask_api/models/cotista_op.py
@@ -4,16 +4,21 @@ import sqlalchemy as db
 
 class CotistaOp(Base):
 
-    __tablename__ = 'cotista_op'
-    
+    __tablename__ = "cotista_op"
+
     IdOperacao = db.Column(db.Integer, primary_key=True)
     IdCotista = db.Column(db.Integer)
     ApelidoCotista = db.Column(db.String)
     IdCarteira = db.Column(db.Integer)
     ApelidoCarteira = db.Column(db.String)
     CnpjCarteira = db.Column(db.String)
-    ApelidoDistribuidor=db.Column(db.String,nullable=True)
-    CnpjDistribuidor=db.Column(db.String,nullable=True)
+    ApelidoDistribuidor = db.Column(db.String, nullable=True)
+    CnpjDistribuidor = db.Column(db.String, nullable=True)
+    ApelidoGestor = db.Column(db.String, nullable=True)
+    CnpjGestor = db.Column(db.String, nullable=True)
+    ApelidoAdministrador = db.Column(db.String, nullable=True)
+    CnpjAdministrador = db.Column(db.String, nullable=True)
+    Serie = db.Column(db.Integer, nullable=True)
     DataOperacao = db.Column(db.Date)
     DataRegistro = db.Column(db.Date)
     DataConversao = db.Column(db.Date)
@@ -21,6 +26,12 @@ class CotistaOp(Base):
     TipoOperacao = db.Column(db.Integer)
     DescricaoTipoOperacao = db.Column(db.String)
     ValorBruto = db.Column(db.Float)
+    ValorIr = db.Column(db.Float)
+    ValorIof = db.Column(db.Float)
     ValorLiquido = db.Column(db.Float)
     Quantidade = db.Column(db.Float)
-    
+    TipoLiquidacao = db.Column(db.String)
+    IdPosicaoResgatada = db.Column(db.Integer, nullable=True)
+    DataAplicCautelaResgatada = db.Column(db.Date, nullable=True)
+    ContaFundo = db.Column(db.String)
+    ContaDistribuidor = db.Column(db.String, nullable=True)
