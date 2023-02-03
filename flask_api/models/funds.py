ede11259da43e384c0af2dd0849d6fd11c11bebd
VitorRussomanoCGCOMPASS <Vitor.Ibanez@cgcompass.com>
2023-02-03 19:01:57 -0300

    nnew models

--- flask_api/models/funds.py
+++ flask_api/models/funds.py
@@ -5,19 +5,42 @@ from sqlalchemy.orm import relationship
 
 class Funds(Base):
     __tablename__ = "funds"
-    
-    britech_id = db.Column(db.Integer, primary_key=True,autoincrement=False)
+
+    britech_id = db.Column(db.Integer, primary_key=True, autoincrement=False)
     cnpj = db.Column(db.String)
     apelido = db.Column(db.String)
-    cotista_id = db.Column(db.Integer,nullable=True)
+    cotista_id = db.Column(db.Integer, nullable=True)
     inception_date = db.Column(db.Date)
-
+    status = db.Column(db.String)
+    name = db.Column(db.String)
+    
     values = relationship("FundsValues")
 
 
 class FundsValues(Base):
     __tablename__ = "funds_values"
 
+    funds_id = db.Column(
+        db.Integer, db.ForeignKey("funds.britech_id"), primary_key=True
+    )
+
     date = db.Column(db.Date, primary_key=True)
-    funds_id = db.Column(db.Integer, db.ForeignKey("funds.britech_id"), primary_key=True)
-    cota = db.Column(db.Float)
+    CotaAbertura = db.Column(db.Float)
+    CotaFechamento = db.Column(db.Float)
+    CotaBruta = db.Column(db.Float)
+    PLAbertura = db.Column(db.Float)
+    PLFechamento = db.Column(db.Float)
+    PatrimonioBruto = db.Column(db.Float)
+    QuantidadeFechamento = db.Column(db.Float)
+    AjustePL = db.Column(db.Float)
+    CotaImportada =db.Column(db.String)
+    CotaEx = db.Column(db.Float)
+    CotaRendimento = db.Column(db.Float)
+    ProventoAcumulado = db.Column(db.Float)
+    IdSerieOffShore = db.Column(db.Integer)
+    
+
+
+
+
+
