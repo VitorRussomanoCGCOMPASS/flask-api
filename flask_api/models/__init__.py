93516d49066099930eb53aaca31c8b6427897378
VitorRussomanoCGCOMPASS <Vitor.Ibanez@cgcompass.com>
2023-03-09 22:30:23 -0300

    init

--- flask_api/models/__init__.py
+++ flask_api/models/__init__.py
@@ -1,8 +1,1 @@
 # TODO : FLASK API MODELS INNIT IMPORT ALL https://github.com/apache/airflow/blob/main/airflow/models/__init__.py
-import flask_api.models.ima
-import flask_api.models.cricra
-import flask_api.models.debentures
-import flask_api.models.vna
-import flask_api.models.indexes
-import flask_api.models.cotista_op
-import flask_api.models.funds
