3497aa52be9fc15a43ab6d5689a1c0d1ed43bcbf
VitorRussomanoCGCOMPASS <Vitor.Ibanez@cgcompass.com>
2023-03-31 14:50:05 -0300

    anbima

---
+++ flask_api/models/anbima/__init__.py
@@ -0,0 +1,4 @@
+from flask_api.models.anbima.cricra import CriCra, TempCriCra
+from flask_api.models.anbima.debentures import Debentures, TempDebentures
+from flask_api.models.anbima.ima import TempIMA, IMA, ComponentsIMA, TempComponentsIMA
+from flask_api.models.anbima.vna import TempVNA, VNA
