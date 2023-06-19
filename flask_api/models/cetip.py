0bc48e86ca443d1475b6aba474fc816dc10524d1
VitorRussomanoCGCOMPASS <Vitor.Ibanez@cgcompass.com>
2023-06-19 19:24:17 -0300

    cetip

--- flask_api/models/cetip.py
+++ flask_api/models/cetip.py
@@ -6,7 +6,7 @@ from sqlalchemy import null
 class CetipBase(Base):
 
     __abstract__  = True
-    type = db.Column(db.String(3))
+    type = db.Column(db.String(10))
     issuer = db.Column(db.String,nullable=True)
     instrument_code = db.Column(db.String(50),primary_key=True)
     price_max = db.Column(db.Float)
@@ -16,13 +16,13 @@ class CetipBase(Base):
     rate_mean = db.Column(db.Float,nullable=True)
     rate_min = db.Column(db.Float,nullable=True)
     quantity = db.Column(db.Float)
-    nof_trades = db.Column(db.Float)
+    nof_trades = db.Column(db.Integer)
     financial_vol = db.Column(db.Float)
     last_price = db.Column(db.Float)
     last_rate = db.Column(db.Float,nullable=True)
-    last_trade_source = db.Column(db.Float)
-    last_trade_closing_time = db.Column(db.Float)
-    last_trade_settlement_date = db.Column(db.Float)
+    last_trade_source = db.Column(db.VARCHAR)
+    last_trade_closing_time = db.Column(db.Time)
+    last_trade_settlement_date = db.Column(db.Date)
     date = db.Column(db.Date,primary_key=True)
 
 
@@ -32,7 +32,19 @@ class Cetip(CetipBase):
 class StageCetip(CetipBase):
     __tablename__ = "stage_" + Cetip.__tablename__
 
-    rate_max = db.Column(db.Float,nullable=True,default=null())
-    rate_mean = db.Column(db.Float,nullable=True,default=null())
-    rate_min = db.Column(db.Float,nullable=True,default=null())
-    last_rate = db.Column(db.Float,nullable=True,default=null())
+    price_max = db.Column(db.VARCHAR)
+    price_mean = db.Column(db.VARCHAR)
+    price_min = db.Column(db.VARCHAR)
+    rate_max = db.Column(db.VARCHAR,nullable=True,default=null())
+    rate_mean = db.Column(db.VARCHAR,nullable=True,default=null())
+    rate_min = db.Column(db.VARCHAR,nullable=True,default=null())
+    quantity = db.Column(db.VARCHAR)
+    nof_trades = db.Column(db.VARCHAR)
+    financial_vol = db.Column(db.VARCHAR)
+    last_price = db.Column(db.VARCHAR)
+    last_rate = db.Column(db.VARCHAR,nullable=True,default=null())
+
+
+    last_trade_closing_time = db.Column(db.VARCHAR)
+    last_trade_settlement_date = db.Column(db.VARCHAR)
+    date = db.Column(db.VARCHAR(50),primary_key=True)
